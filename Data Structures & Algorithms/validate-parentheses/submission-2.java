f6cd27d4d3db6d515ea17feec649fd67367bd4da
aryalnischal82-glitch <aryalnischal82@gmail.com>
2026-05-13 18:15:33 -0500

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack=new Stack<>();
+        for(char c: s.toCharArray()){
+            if(c=='{' || c=='[' || c=='('){
+                stack.push(c);
+            }else{
+                if(stack.isEmpty() || (c=='}' && stack.peek()!='{')){
+                    return false;
+                } 
+                if(stack.isEmpty() || (c==')' && stack.peek()!='(')){
+                    return false;
+
+                } 
+                if(stack.isEmpty() || (c==']' && stack.peek()!='[')){
+                    return false;
+                } 
+                 stack.pop();
+            }
+        }
+        return stack.isEmpty();
+    }
+}
