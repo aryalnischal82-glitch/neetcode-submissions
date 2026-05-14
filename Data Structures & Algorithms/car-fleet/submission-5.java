1269db7a964ae4930c579718f247ee56f0404aa2
aryalnischal82-glitch <aryalnischal82@gmail.com>
2026-05-14 01:21:27 -0500

    Add: car-fleet - submission-5

---
+++ Data Structures & Algorithms/car-fleet/submission-5.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int carFleet(int target, int[] position, int[] speed) {
+        int l=position.length;
+        int[][] cars=new int[l][2];
+        for(int i=0;i<l;i++){
+            cars[i][0]=position[i];
+            cars[i][1]=speed[i];
+        }
+        Arrays.sort(cars,(a,b)->a[0]-b[0]);
+        double lastCarTime=(target-cars[l-1][0])/cars[l-1][1];
+        int fleets=1;
+        for(int i=l-2;i>=0;i--){
+            double currCarTime=(double)(target-cars[i][0])/cars[i][1];
+            if(currCarTime>lastCarTime){
+                fleets++;
+                lastCarTime=currCarTime;
+            }
+        }
+        return fleets;
+    }
+}
