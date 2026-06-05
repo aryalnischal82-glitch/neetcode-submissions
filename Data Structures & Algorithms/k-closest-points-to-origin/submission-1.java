aa2a407ee48d015485432ed4078a9e7c3cc4a95d
aryalnischal82-glitch <aryalnischal82@gmail.com>
2026-06-05 14:20:51 -0500

    Add: k-closest-points-to-origin - submission-1

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-1.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int[][] kClosest(int[][] points, int k) {
+        // we need a max heap of size k 
+    PriorityQueue<Point> pq=new PriorityQueue<>((a,b)->b.dist-a.dist);
+    for(int[] point: points){
+        int dist=point[0]*point[0]+point[1]*point[1];
+        pq.offer(new Point(point,dist));
+        if(pq.size()>k) pq.poll();
+    }
+
+    int[][] res=new int[k][2];
+    int i=0;
+    while(!pq.isEmpty()){
+        res[i++]=pq.poll().point;
+    }
+    return res;
+    }
+}
+
+class Point{
+    int[] point;
+    int dist;
+
+    public Point(int[] point, int dist){
+        this.point=point;
+        this.dist=dist;
+    }
+}
+
+
+
+//Nlog(N)
