7b0c31167a2a7ac0e287f45dca5f4d6c53f71fa8
aryalnischal82-glitch <aryalnischal82@gmail.com>
2026-06-07 01:41:44 -0500

    Add: combination-target-sum - submission-0

---
+++ Data Structures & Algorithms/combination-target-sum/submission-0.java
@@ -0,0 +1,26 @@
+class Solution {
+    int[] nums;
+    int target;
+    List<List<Integer>> res;
+    public List<List<Integer>> combinationSum(int[] nums, int target) {
+        this.nums=nums;
+        this.target=target;
+        this.res=new ArrayList<>();
+        Arrays.sort(nums);
+        dfs(0,0, new ArrayList<>());
+        return res;
+    }
+
+    private void dfs(int idx, int runningSum, List<Integer> curr){
+        if(runningSum==target){
+            res.add(new ArrayList<>(curr));
+            return;
+        }
+        if(runningSum>target) return;
+        for(int i=idx;i<nums.length;i++){
+            curr.add(nums[i]);
+            dfs(i,nums[i]+runningSum,curr);
+            curr.remove(curr.size()-1);
+        }
+    }
+}
