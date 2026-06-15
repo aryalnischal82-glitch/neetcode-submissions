b08652beb91db8434433eef9603d4a886596e8c9
aryalnischal82-glitch <aryalnischal82@gmail.com>
2026-06-14 23:55:34 -0500

    Add: search-for-word-ii - submission-1

---
+++ Data Structures & Algorithms/search-for-word-ii/submission-1.java
@@ -0,0 +1,62 @@
+class Solution {
+    char[][] board;
+    String[] words;
+    List<String> res;
+    Node root;
+    int ROWS;
+    int COLS;
+    public List<String> findWords(char[][] board, String[] words) {
+        this.board=board;
+        this.words=words;
+        this.res=new ArrayList<>();
+        this.root=new Node();
+        this.ROWS=board.length;
+        this.COLS=board[0].length;
+
+        //adding all words to the trie root
+        for(String word: words){
+            Node curr=root;
+            for(char c: word.toCharArray()){
+                if(curr.children[c-'a']==null){
+                    curr.children[c-'a']=new Node();
+                }
+                curr=curr.children[c-'a'];
+            }
+            curr.word=word;
+        }
+        
+        for(int r=0;r<ROWS;r++){
+            for(int c=0;c<COLS;c++){
+                char currChar=board[r][c];
+                if(root.children[currChar-'a']!=null) dfs(root,r,c);
+            }
+        }
+        return res;   
+    }
+
+    private void dfs(Node node, int r, int c){
+        if(node==null) return;
+        if(node.word!=null){
+            res.add(node.word);
+            node.word=null;
+        }
+        if(r<0 || r==ROWS || c<0 || c==COLS || board[r][c]=='#' || (node.children[board[r][c]-'a']==null)) return;
+        char currChar=board[r][c];
+        board[r][c]='#';
+        dfs(node.children[currChar-'a'],r+1,c);
+        dfs(node.children[currChar-'a'],r-1,c);
+        dfs(node.children[currChar-'a'],r,c+1);
+        dfs(node.children[currChar-'a'],r,c-1);
+        board[r][c]=currChar;
+    }
+}
+
+class Node{
+    Node[] children;
+    String word;
+
+    public Node(){
+        this.children=new Node[26];
+        this.word=null;
+    }
+}
