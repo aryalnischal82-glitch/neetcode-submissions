88e1664d09d58f8112bdede4292f3f5f73012dcd
aryalnischal82-glitch <aryalnischal82@gmail.com>
2026-06-20 13:45:15 -0500

    Add: surrounded-regions - submission-1

---
+++ Data Structures & Algorithms/surrounded-regions/submission-1.java
@@ -0,0 +1,42 @@
+class Solution {
+    char[][] board;
+    int ROWS;
+    int COLS;
+    int[][] DIRS;
+    public void solve(char[][] board) {
+        this.board=board;
+        this.ROWS=board.length;
+        this.COLS=board[0].length;
+        this.DIRS=new int[][]{{-1,0},{1,0},{0,-1},{0,1}};
+
+        for(int r=0;r<ROWS;r++){
+            for(int c=0;c<COLS;c++){
+                if((r==0 || r==ROWS-1 || c==0 || c==COLS-1)){
+                   dfs(r,c);
+                }
+            }
+        }
+
+        for(int r=0;r<ROWS;r++){
+            for(int c=0;c<COLS;c++){
+                if(board[r][c]=='O'){
+                    board[r][c]='X';
+                }else if(board[r][c]=='S'){
+                    board[r][c]='O';
+                }
+            }
+        }
+        
+
+    }
+
+    private void dfs(int r, int c){
+        if(r<0 || r>=ROWS || c<0 || c>=COLS || board[r][c]!='O') return;
+        board[r][c]='S';
+        for(int[] DIR: DIRS){
+            int nr=r+DIR[0];
+            int nc=c+DIR[1];
+            dfs(nr,nc);
+        }
+    }
+}
